53414141625eb24c361606271365f1f7b57336ad
duarte <tduarte@etrr.edu.ar>
2025-05-15 10:13:41 -0300

    Declarar y mostrar una variable.c

---
+++ Declarar y mostrar una variable.c
@@ -0,0 +1,11 @@
+//Declarar y mostrar una variable
+
+#include <stdio.h>
+
+int main(){
+    int edad;
+    printf("Ingrese su edad\n");
+    scanf("%d", &edad);
+    
+    printf("Su edad es de %d años", edad);
+}
