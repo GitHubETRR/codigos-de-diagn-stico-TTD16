4e4be0a4db9bef4f340723a08dec14d2dcbec8fd
duarte <tduarte@etrr.edu.ar>
2025-06-26 09:35:43 -0300

    Ex2.c

---
+++ structs/Ex2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+struct Persona{
+    char nombre[50];
+    int edad;
+    float altura;
+};
+
+void cargarpersonas(struct Persona p[], int cantidad){
+    for(int i=0; i<cantidad; i++){
+        printf("\n--Persona %d---\n", i+1);
+        printf("Nombre: ");
+        scanf("%s", p[i].nombre);
+        printf("Edad: ");
+        scanf("%d", &p[i].edad);
+        printf("Altura: ");
+        scanf("%f", &p[i].altura);
+    }
+}
+void mostrarpersonas(struct Persona p[], int cantidad){
+    printf("\nINFORMACION DE PERSONAS...\n");
+    for(int i=0;i<cantidad;i++){
+        printf("\n--Persona %d--\n", i+1);
+        printf("Nombre: %s\n", p[i].nombre);
+        printf("Edad: %d\n", p[i].edad);
+        printf("Altura: %f\n", p[i].altura);
+    }
+}
+int main(){
+    int cantidad;
+    printf("Bienvenido, ingrese cuantas personas quiere ingresar: ");
+    scanf("%d", &cantidad);
+    struct Persona personas[cantidad];    
+
+    cargarpersonas(personas, cantidad);
+    mostrarpersonas(personas, cantidad);
+}
