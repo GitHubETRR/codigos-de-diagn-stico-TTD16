d2b546555da74b0276ecd60b247aa8753402ba95
duarte <tduarte@etrr.edu.ar>
2025-07-17 11:19:31 -0300

    COD.c++

--- Ejercicios con ciclos 1.c
+++
@@ -1,9 +0,0 @@
-//Desarrollar un algoritmo que escriba por pantalla los primeros 100 números naturales.
-
-#include <stdio.h>
-
-int main(){
-    for(int i=0;i<100;i++){
-        printf("%d \n", i);
-    }
-}
