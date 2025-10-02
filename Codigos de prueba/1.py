69d528580a2664e64da1245ee082d007dc0475e7
duarte <tduarte@etrr.edu.ar>
2025-10-02 10:58:49 -0300

    Create 1.py

---
+++ Codigos de prueba/1.py
@@ -0,0 +1,8 @@
+def sumar(num1, num2):
+    return num1 + num2
+
+numero1 = int(input("Ingrese un número: "))
+numero2 = int(input("Ingrese otro número: "))
+
+resultado = sumar(numero1, numero2)
+print("La suma es:", resultado)
